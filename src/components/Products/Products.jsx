0770b8a94e4c27ede1c1fadad753602bcb204287
jovanmit998 <jovanmitrovic998@gmail.com>
2021-06-27 16:02:55 +0200

    Initial commit

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Product from './Product/Product';
+import useStyles from './styles';
+
+/*const products=[
+    { id:1, name: 'Čaj', description: 'Čaj od Ive', price: '100din', image: 'https://starahercegovackakuca.com/wp-content/uploads/2019/12/Iva-674x1024-1.jpg' },
+    { id:2, name: 'Ulje', description: 'Biljno ulje', price: '250din', image: 'https://www.dijamant.rs/wp-content/uploads/2016/03/Dijamant-Mediteran-ulje.jpg' },
+
+
+];*/
+const Products = ({ products, onAddToCart }) => {
+    const classes=useStyles();
+    return(
+    <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+           
+        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+        <Product product={product} onAddToCart={() => onAddToCart(product.id, 1)}/>
+        
+        </Grid>
+        ))}
+        </Grid>
+
+        </main>
+
+    )
+    
+
+}
+export default Products;
